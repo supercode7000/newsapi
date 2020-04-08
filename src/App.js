6936c449685478c951614e309096f7a84ab5890d
supercode7000 <anass@super-code.de>
2020-04-08 10:03:25 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
       News Api
+      <nav>
+        <Link to="/">Deutsch </Link>
+        <Link to="/french"> Français</Link>
+        <Link to="/english">English</Link>
+        <Link to="/ukranien">Ukranien</Link>
+      </nav>
+      <Switch>
+        <Route Path='/' exact>
+          <Test />
+        </Route>
+        <Route Path='/french'>
+          <Test />
+        </Route>
+        <Route Path='/english'>
+          <Test />
+        </Route>
+        <Route Path='/ukranien'>
+          <Test />
+        </Route>
+      </Switch>
     </div>
+
   );
 }
 
